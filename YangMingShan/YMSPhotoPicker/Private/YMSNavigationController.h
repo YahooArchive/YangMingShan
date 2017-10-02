4740f7b44678ca21e533d8d1e64a81ca3223735f
Howard Huang <zzbhhuang@gmail.com>
2017-10-02 09:59:01 +0800

    Add YMSNavigationController for safeArea

---
+++ YangMingShan/YMSPhotoPicker/Private/YMSNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  YMSNavigationController.h
+//  YangMingShanDemo
+//
+//  Copyright 2017 Yahoo Inc.
+//  Licensed under the terms of the BSD license. Please see LICENSE file in the project root for terms.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMSNavigationController : UINavigationController
+
+@end
